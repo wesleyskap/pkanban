fb20da10625f142e2099dcff8fafd48de4ca8aa7
Wesley Conde <wesleyskap@gmail.com>
2013-05-31 15:43:39 -0300

    Refactor sprints_controller. simplifying

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -1,91 +1,56 @@
 class SprintsController < ApplicationController
 
   before_filter :authenticate_user!
- 
-  # GET /sprints
-  # GET /sprints.json
+  before_filter :sprint_find,   :only => [:edit, :update, :destroy]
+  before_filter :validate_user!, :only => [:edit, :update, :destroy]
+
+  respond_to :html, :json
+
   def index
     @sprints = Sprint.where(:User_id => current_user.id)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @sprints }
-    end
+    respond_with @sprints
   end
 
-  # GET /sprints/new
-  # GET /sprints/new.json
   def new
     @sprint = Sprint.new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @sprint }
-    end
+    respond_with @sprints
   end
 
-  # GET /sprints/1/edit
   def edit
     @sprint = Sprint.find(params[:id])
-    
-    # validate 
-    self.validate_user(current_user.id, @sprint.User_id)
   end
 
-  
-  # POST /sprints
-  # POST /sprints.json
   def create
     @sprint = Sprint.new(params[:sprint])
     @sprint.User_id = current_user.id
 
-    respond_to do |format|
-      if @sprint.save
-        format.html { redirect_to :action => 'index' }
-        format.json { render :json => @sprint, :status => :created, :location => @sprint }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @sprint.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Sprint was successfully created." if @sprint.save
+    respond_with @sprint, :location => sprints_path
   end
 
-  # PUT /sprints/1
-  # PUT /sprints/1.json
   def update
     @sprint = Sprint.find(params[:id])
 
-    # validate
-    self.validate_user(current_user.id, @sprint.User_id)
-
-    respond_to do |format|
-      if @sprint.update_attributes(params[:sprint])
-        format.html { redirect_to :action => 'index' }
-        format.json { head :no_content }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @sprint.errors, :status => :unprocessable_entity }
-      end
-    end
+    flash[:notice] = "Sprint was successfully updated." if @sprint.update_attributes(params[:sprint])
+    respond_with @sprint
   end
 
-  # DELETE /sprints/1
-  # DELETE /sprints/1.json
   def destroy
     @sprint = Sprint.find(params[:id])
-    
-    # validate
-    self.validate_user(current_user.id, @sprint.User_id)
-    @sprint.destroy
 
-    respond_to do |format|
-      format.html { redirect_to sprints_url }
-      format.json { head :no_content }
-    end
+    @sprint.destroy
+    respond_with @sprint
   end
-  
-  def validate_user(session_id, user_id)
-    if ( session_id != user_id )
-      redirect_to :action => 'index'
+
+private
+  def sprint_find
+    @sprint = Sprint.find(params[:id])
+  end
+
+  def validate_user!
+    unless current_user.id == @sprint.User_id
+      flash[:notice] = "Sprint not finded"
+      redirect_to sprints_path
     end
   end
 
