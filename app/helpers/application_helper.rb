45ac823519acd4c177a7d32f4e824a71f63ac0d7
Wesley Conde <wesleyskap@gmail.com>
2013-05-31 15:44:51 -0300

    Help flash_messages for layout application.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def flash_messages
+      flash.collect do |key, msg|
+        content_tag(:p, msg, :id => key, :class => "flash_message")
+      end.join.html_safe
+    end
 end
